f1df5e4ceeb2b3e427dbe8909bf1d9e6a1b0a7a4
wouter <wouter.kemps@gmail.com>
2018-05-02 17:49:47 +0200

    interface regression

---
+++ Models/RegressionModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using NoruST.Domain;
+
+namespace NoruST.Models
+{
+	public class RegressionModel
+	{
+	}
+}
---
+++ Presenters/RegressionPresenter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NoruST.Forms;
+using NoruST.Models;
+using NoruST.Presenters;
+using NoruST.Domain;
+using DataSet = NoruST.Domain.DataSet;
+using System.ComponentModel;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Excel;
+
+namespace NoruST.Presenters
+{
+	public class RegressionPresenter
+	{
+		private RegressionForm view;
+		private RegressionModel model;
+		private DataSetManagerPresenter dataSetPresenter;
+
+		public RegressionPresenter(DataSetManagerPresenter presenter)
+		{
+			this.dataSetPresenter = presenter;
+			model = new RegressionModel();
+		}
+
+		public RegressionModel getModel()
+		{
+			return model;
+		}
+
+		public void openView()
+		{
+			view = view.createAndOrShowForm();
+			view.setPresenter(this);
+		}
+
+		public BindingList<DataSet> dataSets()
+		{
+			return dataSetPresenter.getModel().getDataSets();
+		}
+	}
+}
